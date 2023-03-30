590f190f8702ee7905aac35650ff7f7d71458ade
vadimSch <belayapuma4@gmail.com>
2023-03-30 21:27:43 +0300

    add

---
+++ univid-app/src/utils/constants.js
@@ -0,0 +1,46 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import PetsIcon from '@mui/icons-material/Pets';
+import AccessibilitySharpIcon from '@mui/icons-material/AccessibilitySharp';
+import FlightSharpIcon from '@mui/icons-material/FlightSharp';
+import RamenDiningIcon from '@mui/icons-material/RamenDining';
+
+export const logo = '../images/logo_transparent.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'Travel', icon: <FlightSharpIcon/>, },
+  { name: 'Anime', icon: <AccessibilitySharpIcon />, },
+  { name: 'Cooking', icon: <RamenDiningIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Animal', icon: <PetsIcon />, },
+];
+
+export const demoThumbnailUrl = '';
+export const demoChannelUrl = '';
+export const demoVideoUrl = '';
+export const demoChannelTitle = 'Univid';
+export const demoVideoTitle = '';
+export const demoProfilePicture = '';
+
